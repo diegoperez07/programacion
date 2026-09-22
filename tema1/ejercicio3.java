be44f6992793c74da88c78dec6343cdf1edd62a1
diegoperez07 <diegomanuelperezlara@gmail.com>
2026-09-22 13:16:16 +0200

    Add files via upload

---
+++ tema1/ejercicio3.java
@@ -0,0 +1,8 @@
+// añadir mi direccion y numero de telefono
+public class Nombre {
+    public static void main(String[] args) {
+	System.out.println("Diego Manuel Pérez Lara");
+	System.out.println("Dirección:Plaza corteconcepción 4");
+	System.out.println("Télefono:684150333");
+	}
+}
