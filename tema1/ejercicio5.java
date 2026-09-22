dead6ecf4959bfd1ee4bf8d6b28275864db4b32e
diegoperez07 <diegomanuelperezlara@gmail.com>
2026-09-22 14:13:21 +0200

    Update ejercicio5.java

--- tema1/ejercicio5.java
+++ tema1/ejercicio5.java
@@ -1,14 +1,12 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hora\t\tLunes\tMartes\tMierc.\tJueves\tViernes\n");
-    printf("---------------------------------------------------------\n");
-    printf("08:00-09:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tEDDAW\n");
-    printf("09:00-10:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tBDDAW\n");
-    printf("10:00-11:00\tSIDAW\tSOS\tITEC\tLMDAW\tBDDAW\n");
-    printf("11:30-12:30\tSIDAW\tEDDAW\tITEC\tSIDAW\tITEC\n");
-    printf("12:30-13:30\tPRDAW\tPRDAW\tDIG\tBDDAW\tPRDAW\n");
-    printf("13:30-14:30\tPRDAW\tPRDAW\tEDDAW\tBDDAW\tPRDAW\n");
-
-    return 0;
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        System.out.println("Hora\t\tLunes\tMartes\tMierc.\tJueves\tViernes");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("08:00 - 09:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tEDDAW");
+        System.out.println("09:00 - 10:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tBDDAW");
+        System.out.println("10:00 - 11:00\tSIDAW\tSOS\tITEC\tLMDAW\tBDDAW");
+        System.out.println("11:30 - 12:30\tSIDAW\tEDDAW\tITEC\tSIDAW\tITEC");
+        System.out.println("12:30 - 13:30\tPRDAW\tPRDAW\tDIG\tBDDAW\tPRDAW");
+        System.out.println("13:30 - 14:30\tPRDAW\tPRDAW\tEDDAW\tBDDAW\tPRDAW");
+    }
 }
