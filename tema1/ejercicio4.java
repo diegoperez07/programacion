ac498ce6cb67e6bb7bc36dc312c825b95cf5efcc
diegoperez07 <diegomanuelperezlara@gmail.com>
2026-09-22 14:01:45 +0200

    Add files via upload

---
+++ tema1/ejercicio4.java
@@ -0,0 +1,19 @@
+// escribir 10 palabras en ingles con su traducción al castellano
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+	System.out.println("Inglés\t\tCastellano");
+	System.out.println("---------------------");
+	System.out.println("Computer\tordenador");
+	System.out.println("Student\tEstudiante");
+	System.out.println("Keyboard\tTeclado");
+	System.out.println("Screen\tPantalla");
+	System.out.println("Book\tlibro");
+	System.out.println("Code\tCódigo");
+	System.out.println("file\tArchivo");
+	System.out.println("network\tRed");
+	System.out.println("System\tSistema");
+	System.out.println("Window\tVentana");
+	}
+}
+	
---
+++ tema1/ejercicio5.java
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hora\t\tLunes\tMartes\tMierc.\tJueves\tViernes\n");
+    printf("---------------------------------------------------------\n");
+    printf("08:00-09:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tEDDAW\n");
+    printf("09:00-10:00\tLMDAW\tBDDAW\tSIDAW\tPRDAW\tBDDAW\n");
+    printf("10:00-11:00\tSIDAW\tSOS\tITEC\tLMDAW\tBDDAW\n");
+    printf("11:30-12:30\tSIDAW\tEDDAW\tITEC\tSIDAW\tITEC\n");
+    printf("12:30-13:30\tPRDAW\tPRDAW\tDIG\tBDDAW\tPRDAW\n");
+    printf("13:30-14:30\tPRDAW\tPRDAW\tEDDAW\tBDDAW\tPRDAW\n");
+
+    return 0;
+}
